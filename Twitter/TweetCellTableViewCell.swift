1355c6637cecca7d6a3c220cbf15ab2119b8db13
hong <hongwootan@outlook.com>
2021-09-30 17:20:37 -0400

    adding user can compose a tweet and favorite a tweet

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,6 +14,44 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
     
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: { [self] in
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite did not succed: \(error)")
+            })
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if(favorited) {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for:UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named:"favor-icon-1"), for:UIControl.State.normal)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
